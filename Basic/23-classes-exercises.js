a15590642ca3db9f94673d7bdd18add1313d4139
Juanma Sosa <jmasosamendoza@gmail.com>
2024-08-20 16:25:19 +0100

    Update Unit 23, 24 & 25

---
+++ Basic/23-classes-exercises.js
@@ -0,0 +1,24 @@
+/*
+Clases (continuación) y manejo de errores
+
+*/
+
+// 1. Crea una clase que reciba dos propiedades
+
+// 2. Añade un método a la clase que utilice las propiedades
+
+// 3. Muestra los valores de las propiedades e invoca a la función
+
+// 4. Añade un método estático a la primera clase
+
+// 5. Haz uso del método estático
+
+// 6. Crea una clase que haga uso de herencia
+
+// 7. Crea una clase que haga uso de getters y setters
+
+// 8. Modifica la clase con getters y setters para que use propiedades privadas
+
+// 9. Utiliza los get y set y muestra sus valores
+
+// 10. Sobrescribe un método de una clase que utilice herencia 
