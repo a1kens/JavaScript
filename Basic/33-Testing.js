f65867c5c9142acaf0be9b6ba11e0261ece1c3fc
Juanma Sosa <jmasosamendoza@gmail.com>
2024-09-12 14:40:45 +0100

    Update

---
+++ Basic/33-Testing.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3]
+const [x, y, z] = numbers
+// Guess the Outuput?
+console.log(x, y, z) // 123  - La desestructuración de arrays te permite extraer valores de un array y asignarlos a variables de forma más concisa
+
