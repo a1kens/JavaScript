72c8e74dcfcfc28582bb0386a8c929b9c430261e
Juanma Sosa <jmasosamendoza@gmail.com>
2024-09-12 19:57:13 +0100

    Update

--- Basic/33-Testing.js
+++ Basic/33-Testing.js
@@ -3,3 +3,48 @@ const [x, y, z] = numbers
 // Guess the Outuput?
 console.log(x, y, z) // 123  - La desestructuración de arrays te permite extraer valores de un array y asignarlos a variables de forma más concisa
 
+
+
+
+// Ordenación
+const num = [1, 2, 3, 4, 5, 6]
+
+
+const numD = [1, 4, 6, 3, 2, 4];
+const numOrder = [1, 4, 6, 3, 2, 5];
+
+
+numOrder.sort((a, b) => a - b) // Orden ascendente
+console.log(`Ordenando el numOrder en orden ascendente: `, numOrder);
+
+numOrder.sort((a,b) => b - a) // Orden descendente
+console.log(`Ordenando el numOrder en orden descendente: `, numOrder);
+
+
+
+// const numD = [1, 4, 6, 3, 2, 4];
+numD.sort((a, b) => a - b);
+// console.log(numD);  // [1, 2, 3, 4, 4, 6]
+//  console.log(`Ordenando el numD: `, numD);
+
+
+// Loop  for, While - Cual utilizar
+
+
+// Manipulación de Arrays - Métodos de JavaScript(agregar, eliminar, split..)
+
+
+
+// Todos los algoritmos tienen una forma para poderse desarrollar
+
+
+/*
+Complejidad -
+Recursos que nuestro ordenador está consumiendo
+Codigo que sea escalable y se pueda mantener en el tiempo, tiene que ser legible
+Complejidad Temporal: número de operaciones (operaciones math., comparaciones, funciones...etc)
+Complejidad Espacial: Memoria necesaria
+Notación Big O: Que tan rápido es
+
+
+*/
